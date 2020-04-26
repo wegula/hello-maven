eda24a6602662d413c906ccf899aae416db61dcd
Agnieszka Wegrzyn <wegrzynagnieszka94@gmail.com>
2020-04-26 14:15:41 +0200

    hello world pseudo text

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,12 +1,26 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
+
 /**
  * Simple main application.
  */
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        builder.width(120).height(20);
+        builder.element(new PseudoText("Hello World!"));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
     }
 
 }
